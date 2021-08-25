6d136bed5b78f2a41f7536a610a5593b468b2d74
Rukiye Çadırcı <83542142+rukicad@users.noreply.github.com>
2021-08-25 09:34:34 +0300

    Update posts.php
    "Homework has been uploaded to the system"

--- posts.php
+++ posts.php
@@ -1,22 +1,15 @@
 <?php
 
 /**
- * posts.php
- *
- * Bu betik direk olarak erişilebilir. functions.php'de yaptığınız gibi bir
- * kontrol eklemenize gerek yok.
- *
- * Bu betik dosyası içerisinde functions.php'de yer alan fonksiyonları da kullanarak
- * aşağıdaki işlemleri gerçekleştirmenizi bekliyoruz.
- *
- * - functions.php içerisinde oluşturacağınız `getRandomPostCount` fonksiyonunuza min
- * ve max değerleri gönderip bu fonksiyondan rastgele bir tam sayı elde etmeniz
- * gerekiyor. (min ve max için istediğiniz değerleri seçebilirsiniz. Random bir
- * tam sayı elde etmek için `rand` (https://www.php.net/manual/en/function.rand.php)
- * fonksiyonunu kullanabilirsiniz.)
- *
- * - Elde ettiğiniz bu sayıyı da kullanarak `getLatestPosts` fonksiyonunu
- * çalıştırmalısınız. Bu fonksiyondan aldığınız diziyi kullanarak `post.php` betik
- * dosyasını döngü içinde dahil etmeli ve her yazı için detayları göstermelisiniz.
- */
+ * posts.php */
 
+include_once("functions.php"); 
+
+// 1 ila 15 arasında rasgele sayı döndürür
+$rand_post_count = getRandomPostCount(1, 15); 
+// rasgele post sayı değerine göre son postları dizi olarak getirir
+$posts = getLatestPosts($rand_post_count); 
+
+  foreach ($posts as $id => $post) {
+      include('post.php'); }
+  
